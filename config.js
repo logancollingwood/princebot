356168bdfe1776efb3205d4af569f4ec057c204f
Logan Collingwood <lcollingwood@blizzard.com>
2016-12-13 13:52:37 -0800

    Fixing msised files

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    'token': 'MjU4Mjk3NzU3OTgxMzQzNzQ0.CzHXBg.cvKC9r2JH3uaSAcajRNn5QOD3kQ',
+    'prefix': '!'
+};
---
+++ controllers/api.js
@@ -0,0 +1,11 @@
+const apiBase = '/api/v1/';
+
+module.exports.set = function(app) {
+	app.get(apiBase + 'doc', function(req, res) {
+		res.send('Hello, world!');
+	});
+
+	app.get(apiBase + 'connect/:inviteLink', function(req,res) {
+	   res.send(req.params);
+    });
+}
